7cfae83812f49a5e675d0a9c3953ab04dbbf03c1
Jeremy Guillette <jaguillette@gmail.com>
2013-04-19 20:46:01 -0300

    Update HathiDataGet.py

--- HathiDataGet.py
+++ HathiDataGet.py
@@ -16,7 +16,7 @@ def getURLparts():
     URLpartsTemp=[]    #defines an empty temporary list to hold query results
     urlGet0 = requests.get(URL, params=Q)
     r = re.search('of \<span class=\"strong\"\>(.*?)\<', urlGet0.text)
-    pages = ceil(int(r.group(1))/20)+2
+    pages = int(ceil(int(r.group(1))/20)+2)
     for n in range(1,pages):    #iterates over the results pages
         Q['page']=n    #sets page in results equal to the iterative value
         urlGet = requests.get(URL, params=Q)    #uses requests module to retrieve search results page
